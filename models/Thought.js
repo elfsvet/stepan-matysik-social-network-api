3944c021b0bfac1697d8e57eca6bfe550573391a
elfsvet <stepanmatysik@gmail.com>
2022-07-07 23:13:04 -0400

    created thought model and started Reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: "Provide the text",
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // TODO use a getter method to format the timestamp on query
+        get: (createAtVal) => dateFormat(createAtVal)
+    },
+    username:{
+        type: String,
+        required: "Provide the username of the author"
+    },
+    reactions: [reactionSchema]
+})
+
+ThoughtSchema.virtual('reactionCount',function(){
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
